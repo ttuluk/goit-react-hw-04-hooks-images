7b135175057e999a138adffe4b26f247e1f6c944
ttuluk <ttasya.k@gmail.com>
2021-11-23 11:22:21 +0200

    add hooks

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,34 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ImageGalleryItem from "./ImageGalleryItem";
 import styles from "./ImageGallery.module.css";
 import imageApi from "../../services/api";
 import Modal from "../Modal/Modal";
 import Button from "../Button/Button";
 
-class ImageGallery extends Component {
-  state = {
-    imageElem: null,
-    showModal: false,
-    imageModal: null,
-    page: 1,
+export default function ImageGallery({openModal}) {
+  const [imageElem, setImageElemState] = useState(null);
+  const [page, setPageState] = useState(1);
+  const [imageModal, setImageModalState] = useState(null);
+  const [showModal, setShowModalState] = useState(false);
+
+  handleLoad = () => {
+    setPageState(prevState => prevState + 1 );
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevProps.searchName;
-    const nextName = this.props.searchName;
-    const pageNumber = this.state.page;
-    const prevPage = prevState.page;
-
-    if (prevName !== nextName || prevPage !== pageNumber) {
-      imageApi
-        .fetchImage(nextName, pageNumber)
-        .then((imageElem) => this.setState({ imageElem: imageElem.hits }))
-        .catch((error) => console.log(error));
-    }
-  }
-
   toggleModal = () => {
-    this.setState((state) => ({ showModal: !state.showModal }));
+    setShowModalState( !showModal );
   };
 
   openModal = (imageId) => {
@@ -42,15 +30,7 @@ class ImageGallery extends Component {
     }));
   };
 
-  handleLoad = () => {
-    this.setState((prevState) => {
-      return { page: prevState.page + 1 };
-    });
-  };
-
-  render() {
-    const { imageElem, showModal, imageModal, loading } = this.state;
-    return (
+  return (
       <>
         <ul className={styles.gallery}>
           {imageElem && (
@@ -67,11 +47,78 @@ class ImageGallery extends Component {
           </Modal>
         )}
         {imageElem && (
-          <Button onClickLoad={this.handleLoad} title={"Load more"} />
+          <Button onClickLoad={handleLoad} title={"Load more"} />
         )}
       </>
-    );
-  }
+    ); 
 }
+// class ImageGallery extends Component {
+//   state = {
+//     imageElem: null,
+//     showModal: false,
+//     imageModal: null,
+//     page: 1,
+//   };
 
-export { ImageGallery };
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevName = prevProps.searchName;
+//     const nextName = this.props.searchName;
+//     const pageNumber = this.state.page;
+//     const prevPage = prevState.page;
+
+//     if (prevName !== nextName || prevPage !== pageNumber) {
+//       imageApi
+//         .fetchImage(nextName, pageNumber)
+//         .then((imageElem) => this.setState({ imageElem: imageElem.hits }))
+//         .catch((error) => console.log(error));
+//     }
+//   }
+
+//   toggleModal = () => {
+//     this.setState((state) => ({ showModal: !state.showModal }));
+//   };
+
+//   openModal = (imageId) => {
+//     const imageModal = this.state.imageElem.find((image) => {
+//       return image.id.toString() === imageId;
+//     });
+
+//     this.setState((state) => ({
+//       showModal: !state.showModal,
+//       imageModal: imageModal.largeImageURL,
+//     }));
+//   };
+
+//   handleLoad = () => {
+//     this.setState((prevState) => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+
+//   render() {
+//     const { imageElem, showModal, imageModal, loading } = this.state;
+//     return (
+//       <>
+//         <ul className={styles.gallery}>
+//           {imageElem && (
+//             <ImageGalleryItem images={imageElem} onOpen={this.openModal} />
+//           )}
+//         </ul>
+//         {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <img
+//               src={imageModal}
+//               alt={imageElem.tags}
+//               className={styles.modal_image}
+//             ></img>
+//           </Modal>
+//         )}
+//         {imageElem && (
+//           <Button onClickLoad={this.handleLoad} title={"Load more"} />
+//         )}
+//       </>
+//     );
+//   }
+// }
+
+// export { ImageGallery };
--- src/components/ImageGallery/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./ImageGalleryItem.module.css";
 
-export default function ImageGalleryItem (onOpen, images) {
+export default function ImageGalleryItem ({onOpen, images}) {
   
   const handleImageClick = (event) => {
   const id = event.currentTarget.id;
@@ -9,13 +9,13 @@ export default function ImageGalleryItem (onOpen, images) {
   };
 
   return (<>
-         {this.props.images.map((image) => {
+         {images.map((image) => {
           return (
             <li
               key={image.id}
               id={image.id}
               className={styles.gallery_item}
-              onClick={this.handleImageClick}
+              onClick={handleImageClick}
             >
               <img
                 src={image.webformatURL}
